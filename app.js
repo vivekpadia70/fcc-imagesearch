923e08c27dde3e085d354e43c22341de5433c91b
vivek padia <vivekpadia70@gmail.com>
2017-04-08 17:12:50 +0530

    v1.0

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var image = require('google-images');
+var getJSON = require('get-json');
+var mongo = require('mongodb').MongoClient;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -13,7 +15,9 @@ var app = express();
 
 var url = "https://www.googleapis.com/customsearch/v1?key=AIzaSyCxJUGIb_tevRKD-Kxxi57OM1f6dbRLwf4&cx=010407088344546736418:onjj7gscy2g&searchType=image"
 var client = new image('010407088344546736418:onjj7gscy2g', 'AIzaSyCxJUGIb_tevRKD-Kxxi57OM1f6dbRLwf4')
+var dbURL = "mongodb://vivekpadia:webbergen@ds145370.mlab.com:45370/sites"
 var queue = {};
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,18 +34,42 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-app.get('/api/imagesearch/:search/:off', function(req, res){
-  var search = req.params.search
-  var off = req.params.off
-  console.log(search)
-  client.search(search).then(item=>{
-    queue = item
-    for(var i=0; i<off; i++){
-      res.writeContinue(queue[i]);
+app.get('/api/imagesearch/:query', function(req, res){
+  var query = req.originalUrl
+  var sub = query.split('/')[3]
+  var values = sub.split('?')
+  var d = Date();
+  mongo.connect(dbURL, function(err, db){
+    var simage = db.collection('simage');
+    simage.insert({term : values[0], when : d})
+    db.close();
+  })
+  url = url + '&q=' + values[0] + '&num=' + Number(values[1]);
+  getJSON(url, function(err, data){
+    var arr = [];
+    for(var i=0; i<Number(values[1]); i++){
+      arr[i]=
+      {
+        'url' : data.items[i].link,
+        'thumbnail' : data.items[i].image.thumbnailLink,
+        'context' : data.items[i].image.contextLink,
+        'snippet' : data.items[i].snippet
+      }
     }
-    res.end()
+    res.send(JSON.stringify(arr));
   })
 })
+// get the data searched from database
+
+app.get('/api/all/imagesearch', function(req, res){
+  mongo.connect(dbURL, function(err, db){
+    var simage = db.collection('simage');
+    simage.find({}).toArray(function(err, data){
+      res.send(data)
+    })
+  })
+})
+
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
